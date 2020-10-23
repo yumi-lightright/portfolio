ade25cce2d52fcc0eb85e7af231f07d7f64f8084
yumi-lightright <lightright0830@gmail.com>
2020-10-23 19:34:37 +0900

    not-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  GET     /Users/yu-mi/projects/portfolio/app/views/layouts/index
 end
